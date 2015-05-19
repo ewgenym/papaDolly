da46eb43afe321ae709c912f60cce853c0377ece
Yevgeniy Maksak <maksak@yandex.ru>
2015-05-19 22:50:08 +0300

    Repeater module: use sleep mode with RFM12 wakeup timer to save power

--- repeater/main.c
+++ repeater/main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <avr/sleep.h>
 #include <avr/wdt.h>
 
 #include "rfm12.h"
@@ -42,25 +43,38 @@ int main(void)
 
 		// Disable unused modules
 		ADCSRA &= ~(1<<ADEN); 	 //Disable ADC
-		ACSR |= (1 << ACD);     // Disable Analog Comparator
-		wdt_disable();			 // Disable Watchdog Timer
+		ACSR |= (1 << ACD);     //Disable Analog Comparator
+		wdt_disable();			 //Disable Watchdog Timer
 
         sei();           //interrupts on
+		
+		//set the wakeup timer to 4 * (2^8)ms = 1.024s
+		rfm12_set_wakeup_timer(0x804);
+
+		//set AVR sleep mode
+		set_sleep_mode(SLEEP_MODE_IDLE);
+		sleep_enable();
 		
 		_blink();
         
         while (1)
         {
-			if (rfm12_rx_status() == STATUS_COMPLETE)
+			//sleep while receiving
+			while(!ctrl.wkup_flag) 
 			{
-				//so we have received a message
-				rfm12_rx_buffer();
+				sleep_mode();
+				if (rfm12_rx_status() == STATUS_COMPLETE)
+				{
+					//so we have received a message
+					rfm12_rx_buffer();
 				
-				rfm12_tx (sizeof(tv), 0xEE, tv);
-				rfm12_tick();
-				rfm12_rx_clear();
-				_delay_ms(1);
-				_blink();
+					rfm12_tx (sizeof(tv), 0xEE, tv);
+					rfm12_tick();
+					rfm12_rx_clear();
+					_delay_ms(1);
+					_blink();				
+				}
 			}
+			ctrl.wkup_flag = 0;
         };
 }
